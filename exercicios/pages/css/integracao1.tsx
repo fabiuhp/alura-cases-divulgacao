02ff547d3f93ece2b72f5ec4a1fa6508d10104a0
fabio <fabiuhp@msn.com>
2023-05-16 16:22:18 -0300

    css modules

---
+++ exercicios/pages/css/integracao1.tsx
@@ -0,0 +1,17 @@
+import styles from './integracao1.module.css'
+
+export default function integracao() {
+    return (
+        <>
+            <div className={styles.vermelha}>
+                Texto #01
+            </div>
+            <div className={styles.azul}>
+                Texto #02
+            </div>
+            <div className={styles.branca}>
+                Texto #03
+            </div>
+        </>
+        )
+}
