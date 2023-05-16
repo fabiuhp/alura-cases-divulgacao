31b6270c0abb4898a32ac7db43b03b65398d2181
fabio <fabiuhp@msn.com>
2023-05-16 09:28:15 -0300

    inicio

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>FAQ</h1>
+            <p>Essa é a página de FAQ</p>
+            <Link href="/">
+                Ir para a home
+            </Link>
+        </div>
+    )
+}
